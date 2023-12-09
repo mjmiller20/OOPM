41f14380dbef7730b3f07460796934c99b651997
JoelASeepersaud <140928189+JoelASeepersaud@users.noreply.github.com>
2023-12-08 18:24:28 -0800

    Delete SSE/font.py

--- SSE/font.py
+++
@@ -1,7 +0,0 @@
-import dearpygui.dearpygui as dpg
-
-def add_fonts(h):
-    with dpg.font_registry():
-        dpg.add_font(tag="Res_font", file="Arial.otf", size=h*.030)
-        dpg.add_font(tag="title_font", file="Arial.otf", size=h*.05)
-
