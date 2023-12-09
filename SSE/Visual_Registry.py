73244cc90f51a4c02b83f3fc5bdb01ed81a44625
JoelASeepersaud <140928189+JoelASeepersaud@users.noreply.github.com>
2023-12-08 18:27:26 -0800

    Add files via upload

--- SSE/Visual_Registry.py
+++ SSE/Visual_Registry.py
@@ -1,6 +1,6 @@
 import dearpygui.dearpygui as dpg
 
-def new_registry(h):
+def visual_registry(h):
     '''fonts'''
     with dpg.font_registry():
         dpg.add_font(tag="Res_font", file="Arial.otf", size=h*.030)
